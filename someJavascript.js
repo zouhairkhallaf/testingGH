68c6cf9c40ff2c2b42f64d04c5ed3d2c4a884c3c
zouhairkhallaf <zouhairkhallaf@gmail.com>
2018-05-12 18:40:36 -0400

    first change

--- someJavascript.js
+++ someJavascript.js
@@ -9,3 +9,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+const message = "this is the first change submitted from branch1"
